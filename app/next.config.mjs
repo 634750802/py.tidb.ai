25103be64491f04151e023cdfbfb3a81e128beae
Jagger <634750802@qq.com>
2024-07-06 01:03:42 +0800

    support docker

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -2,6 +2,7 @@ import withSvgr from 'next-plugin-svgr';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withSvgr({
+  output: process.env.STANDALONE ? 'standalone' : undefined,
   transpilePackages: ['monaco-editor'],
   experimental: {
     optimizePackageImports: ['ai', 'lucide-react'],
